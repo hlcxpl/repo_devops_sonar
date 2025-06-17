7689691223f5d513ee27722e9b7126c4a7187e0c
hlcxpl <hlcxpl@gmai.com>
2025-06-16 23:52:33 -0400

    update4

--- src/main/java/com/equipo/validador/App.java
+++ src/main/java/com/equipo/validador/App.java
@@ -1,10 +1,14 @@
 package com.equipo.validador;
 
+import java.util.logging.Logger;
+
 public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
         String usuario = System.getenv("APP_USER");
         if ("admin".equals(usuario)) {
-            System.out.println("¡Bienvenido administrador!");
+            logger.info("¡Bienvenido administrador!");
         }
     }
 }
